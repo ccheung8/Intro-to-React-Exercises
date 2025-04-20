ee9d154179d0ae163772aeffb1d545743a15fbf6
ccheung8 <c4cheung@ucsd.edu>
2025-04-19 17:16:34 -0700

    started profile component

--- intro-to-react-exercises/src/App.jsx
+++ intro-to-react-exercises/src/App.jsx
@@ -3,11 +3,20 @@ import { GreetingComponent } from './components/GreetingComponent';
 
 function App() {
   const [name, setName] = React.useState();
+  const [user, setUser] = React.useState();
 
   function handleName(event) {
     event.preventDefault();
     setName(event.target.name.value);
     event.target.name.value = "";
+  }
+
+  function generateUser() {
+    fetch('https://randomuser.me/api')
+      .then(res => { return res.json(); })
+      .then((json) => {
+        console.log(json.results[0]);
+      });
   }
 
   return (
@@ -18,6 +27,17 @@ function App() {
           handleName={handleName}
           name={name}
         />
+
+        <div className='profileContainer'>
+          <h2>Static Profile Card</h2>
+          <div className='profile'>
+            <img src="" alt="" />
+            <h3>User Name</h3>
+            <p>some description</p>
+          </div>
+          <button onClick={generateUser}>Generate User</button>
+        </div>
+
 
     </>
   )
--- intro-to-react-exercises/src/components/GreetingComponent.jsx
+++ intro-to-react-exercises/src/components/GreetingComponent.jsx
@@ -1,7 +1,7 @@
 export function GreetingComponent({ handleName, name }) {
   return (
     <div className='greetingContainer'>
-        <h2>Greeting Component</h2>
+      <h2>Greeting Component</h2>
       <form onSubmit={handleName}>
         <label htmlFor='name'>Enter Your Name:</label>
         <input type="text" name='name' id='name' style={{height: "min-content"}} />
